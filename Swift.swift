98584a7649a0fb54af21c0fb8975ec4c178e5a20
jobayer sozib <zeni.sozibndc@gmail.com>
2016-12-26 19:46:34 +0000

    add swift.swift file

---
+++ Swift.swift
@@ -0,0 +1,50 @@
+class Mycode{
+
+/*
+Read-only property
+Return the value of PI=3.1416
+
+*/
+	class var PI:Float{
+		return 3.1416
+	}
+
+
+
+/*
+function Greatest common divisior(gcd)
+@prama (Int a, Int b)
+@return GCD of a and c an Int 
+*/
+
+	func gcd(var a:Int,var b:Int)->Int{
+		if (a%b)==0{
+			return b
+		}else{
+			return self.gcd(b,b:a%b)
+		}
+	}
+
+/*
+Function floor
+@param Double num
+@return convert num to double  
+*/
+	
+	func floor(var num:Double)->Int{
+		return Int(a)
+	}
+	
+	
+/*
+Function ceil
+@param Double num
+@return the least integer that is greater than or equal to @param num
+*/
+	
+	func floor(var num:Double)->Int{
+		return Int(a)
+	}
+
+
+}
